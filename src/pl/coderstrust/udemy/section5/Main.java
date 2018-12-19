3357b2d930a559ff8d1628ba9785555bb6fa9a1e
Tomek Wrobel <wrobelt1@gmail.com>
2018-12-19 22:40:31 +0100

    Initialize commit

---
+++ src/pl/coderstrust/udemy/section5/Main.java
@@ -0,0 +1,31 @@
+package pl.coderstrust.udemy.section5;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(area(-1));
+        System.out.println(area(0));
+        System.out.println(area(5));
+
+        System.out.println(area(-1, 2));
+        System.out.println(area(2, -1));
+        System.out.println(area(2, 2));
+        System.out.println(area(0, 2));
+    }
+
+    public static double area(double radius) {
+        if (radius < 0) {
+            return -1;
+        }
+
+        return radius * radius * 3.14159;
+    }
+
+    public static double area(double x, double y) {
+        if (x < 0 || y < 0) {
+            return -1;
+        }
+
+        return x * y;
+    }
+}
